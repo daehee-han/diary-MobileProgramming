0e66fabb70b9125f9e06c539850c9624158111dd
20163140 <dltnwls8751@gmail.com>
2018-11-14 16:26:32 +0900

    Remove titlebar Add toolbar Change Thema

--- Diary-mobile-programming/app/src/main/java/com/kmu/diary/MainActivity.java
+++ Diary-mobile-programming/app/src/main/java/com/kmu/diary/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;  //버전 맞춰서 import
+
+import com.kmu.diary.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,17 +17,18 @@ public class MainActivity extends AppCompatActivity {
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+        // 화면에 글자 띄워줌 (하단바 말고 주화면에)
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_calendar:
+                    mTextMessage.setText(R.string.title_calendar);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_search);
                     return true;
             }
             return false;
@@ -35,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
