cf04c45c9b1b5153bc5bae76e16559a15664ec14
20163140 <dltnwls8751@gmail.com>
2018-11-20 11:25:39 +0900

    앱 잠금기능 추가

--- Diary-mobile-programming/app/src/main/java/com/kmu/diary/SplashActivity.java
+++ Diary-mobile-programming/app/src/main/java/com/kmu/diary/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.kmu.diary;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -19,17 +21,34 @@ import com.kmu.diary.R;
 
 public class SplashActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 2000;
+    String password;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable(){
+
+        SharedPreferences settings = getSharedPreferences("PREFS", 0);
+        password = settings.getString("password", "");
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
             @Override
             public void run(){
-                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(mainIntent);
-                finish();
+                if(password.equals("")){
+                    // there is no password
+                    Intent mainIntent = new Intent(getApplicationContext(), CreatePasswordActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+                else{
+                    // there is a password
+                    Intent mainIntent = new Intent(getApplicationContext(), EnterPasswordActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+
+                }
+
 
             }
 
