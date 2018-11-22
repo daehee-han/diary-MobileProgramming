01cd746ff43b851f9ce4914cc1281d9f4433df3e
20163140 <dltnwls8751@gmail.com>
2018-11-22 21:59:52 +0900

    renamed SplashActivitiy.java -> LoadingActivity.java renamed activity_splash.xml -> activity_loading.xml modified loding activity design

--- Diary-mobile-programming/app/src/main/java/com/kmu/diary/LoadingActivity.java
+++ Diary-mobile-programming/app/src/main/java/com/kmu/diary/LoadingActivity.java
@@ -1,32 +1,20 @@
 package com.kmu.diary;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.support.v7.widget.Toolbar;  //버전 맞춰서 import
-import android.support.v4.app.Fragment;
-import android.widget.Toast;
 
-import com.kmu.diary.R;
-
-public class SplashActivity extends AppCompatActivity {
+public class LoadingActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 2000;
     String password;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_loading);
 
         SharedPreferences settings = getSharedPreferences("PREFS", 0);
         password = settings.getString("password", "");
